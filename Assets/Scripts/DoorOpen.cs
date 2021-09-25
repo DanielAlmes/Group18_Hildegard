0c94b7f8d48689372dbbea5e9d1d8f633cf34390
Fabienne <fakock@uos.de>
2021-09-26 00:31:28 +0200

    Trying to assimilate  a door

---
+++ Assets/Scripts/DoorOpen.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DoorOpen : MonoBehaviour
+{
+    public float _distance;
+
+    public GameObject ActionText;
+    public GameObject Door;
+    public AudioSource CreakSound;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        _distance = PlayerCasting.DistanceFromTarget;
+
+    }
+
+    private void OnMouseOver()
+    {
+        if (_distance <= 3f)
+        {
+            ActionText.SetActive(true);
+        }
+
+        if (Input.GetButtonDown("Action"))
+        {
+            if (_distance <= 3f)
+            {
+                this.GetComponent<BoxCollider>().enabled = false;
+                ActionText.SetActive(false);
+                Door.GetComponent<Animation>().Play("DoorOpenAnim");
+                CreakSound.Play();
+            }
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        ActionText.SetActive(false);
+    }
+}
