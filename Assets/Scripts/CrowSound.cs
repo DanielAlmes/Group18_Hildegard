7ae3e010a0897c16695d137ec5c02445a51f4250
Fabienne <fakock@uos.de>
2021-09-29 23:04:34 +0200

    Adding craw Sound triggers

---
+++ Assets/Scripts/CrowSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrowSound : MonoBehaviour
+{
+    public AudioSource CreakSound;
+    public void OnTriggerEnter(Collider other)
+    {
+        
+        CreakSound.Play ();
+
+    }
+}
--- Assets/Scripts/Sequences/LevelManager.cs
+++ Assets/Scripts/Sequences/LevelManager.cs
@@ -11,20 +11,20 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private GameObject textBox;
     public void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
             Cursor.visible = false;
             player.GetComponent<PlayerController>().enabled = false;
             StartCoroutine(ScenePlayer());
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-        
+
     }
     IEnumerator ScenePlayer()
     {
         yield return new WaitForSeconds(1f);
-        textBox.GetComponent<Text>().text = "That is the exit!";
-        yield return new WaitForSeconds(2f);
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            textBox.GetComponent<Text>().text = "That is the exit!";
+            yield return new WaitForSeconds(2f);
+        }
         fadeOutScreen.SetActive(true);
         player.GetComponent<PlayerController>().enabled = false;
     }
