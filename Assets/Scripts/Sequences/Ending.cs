7938d4f004ab1fbcf81c4b11e61dbaaf578ce7f9
Fabienne <fakock@uos.de>
2021-09-29 22:04:28 +0200

    FadeOut

---
+++ Assets/Scripts/Sequences/Ending.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ending : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject fadeOutScreen;
+    [SerializeField] private GameObject textBox;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        player.GetComponent<PlayerController>().enabled = false;
+        StartCoroutine(ScenePlayer());
+    }
+
+    IEnumerator ScenePlayer()
+    {
+        yield return new WaitForSeconds(1f);
+        textBox.GetComponent<Text>().text = "That is the exit!";
+        yield return new WaitForSeconds(2f);
+        fadeOutScreen.SetActive(true);
+        player.GetComponent<PlayerController>().enabled = false;
+    }
+}
