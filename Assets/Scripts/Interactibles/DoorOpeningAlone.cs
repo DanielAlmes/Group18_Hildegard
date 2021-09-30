f921b0decc54a8fd2a217a5c7350b04f07a7fcaa
Fabienne <fakock@uos.de>
2021-09-30 11:05:48 +0200

    Add self-opening door

---
+++ Assets/Scripts/Interactibles/DoorOpeningAlone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DoorOpeningAlone : MonoBehaviour
+{
+    public GameObject ActionText;
+    public GameObject Door;
+    public AudioSource CreakSound;
+    public AudioSource SlamSound;
+    
+    //Open the door by itself on entering the trigger
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+                    ActionText.SetActive(false);
+                    Door.GetComponent<Animation>().Play("DoorOpenAnim001");
+                    CreakSound.Play();
+                    Cursor.visible = false;
+        }
+
+    }
+    
+    public void OnTriggerExit(Collider other)
+    {
+                ActionText.SetActive(false);
+                Door.GetComponent<Animation>().Play("DoorSlamCloseAnim001");
+                SlamSound.Play();
+                Cursor.visible = false;
+
+    }
+}
+
